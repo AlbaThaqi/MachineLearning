f4e97f77dd490533e2587c137a5136a5b5462ac5
Alba Thaqi <112955653+AlbaThaqi@users.noreply.github.com>
2025-03-08 09:09:27 +0100

    Categorical_columns

--- Code/machineLearning.py
+++ Code/machineLearning.py
@@ -3,3 +3,7 @@ import numpy
 
 dataset = pandas.read_csv("dataset/alb-rainfall-adm2-full.csv")
 print(dataset.head())
+# print(dataset.info())
+
+categorical_columns = [column for column in dataset.columns if dataset[column].dtype=='object']
+# print("Categorical columns: ",categorical_columns)

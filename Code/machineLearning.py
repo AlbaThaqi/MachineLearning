32083691729d42817fc384ec2ac8ef9a2f1ef955
Alba Thaqi <112955653+AlbaThaqi@users.noreply.github.com>
2025-03-11 09:35:02 -0400

    Convert numerical columns to numeric type and handle missing values

--- Code/machineLearning.py
+++ Code/machineLearning.py
@@ -12,3 +12,13 @@ numerical_columns = [column for column in dataset.columns if dataset[column].dty
 # print("Numerical columns: ",numerical_columns)
 
 print("Unique values: ",dataset[categorical_columns].nunique)
+
+numerical_columns = ["n_pixels", "rfh", "rfh_avg", "r1h", "r1h_avg", "r3h", "r3h_avg", "rfq", "r1q", "r3q"]
+for column in numerical_columns:
+    dataset[column] = pandas.to_numeric(dataset[column],errors='coerce')
+
+# print(dataset.dtypes)
+for column in numerical_columns:
+    print(f"Unique values in {column}: ", dataset[column].unique())
+
+dataset.replace({'?': None, 'missing': None, 'N/A': None}, inplace=True)

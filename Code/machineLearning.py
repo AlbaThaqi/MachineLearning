462ca7595bf61229e526f3b97c7225159eefcf56
Alba Thaqi <112955653+AlbaThaqi@users.noreply.github.com>
2025-03-14 22:58:54 +0100

    "Handle missing values with median imputation

--- Code/machineLearning.py
+++ Code/machineLearning.py
@@ -26,3 +26,10 @@ dataset.replace({'?': None, 'missing': None, 'N/A': None}, inplace=True)
 
 numerical_columns = [col for col in dataset.columns if dataset[col].dtype in ['int64', 'float64']]
 print("Numerical columns: ", numerical_columns)
+
+# print(dataset.isnull().sum())
+
+dataset[['n_pixels', 'rfh', 'rfh_avg', 'rfq']] = dataset[['n_pixels', 'rfh', 'rfh_avg', 'rfq']].fillna(dataset[['n_pixels', 'rfh', 'rfh_avg', 'rfq']].median())
+dataset[['r1h', 'r1h_avg', 'r1q']] = dataset[['r1h', 'r1h_avg', 'r1q']].fillna(dataset[['r1h', 'r1h_avg', 'r1q']].median())
+dataset[['r3h', 'r3h_avg', 'r3q']] = dataset[['r3h', 'r3h_avg', 'r3q']].fillna(dataset[['r3h', 'r3h_avg', 'r3q']].median())
+

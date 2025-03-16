9bc565468730d6dfd0895afcb27deb8fa8bc8e73
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2025-03-16 20:09:03 +0000

    Update machineLearning.py

--- Code/machineLearning.py
+++ Code/machineLearning.py
@@ -66,8 +66,8 @@ for column in numerical_columns:
 # columns_to_transform = ["rfh", "rfh_avg", "r1h", "r1h_avg", "r3h", "r3h_avg", "rfq", "r1q", "r3q"]
 # dataset[columns_to_transform] = dataset[columns_to_transform].apply(lambda x: numpy.log1p(x))
 
-# scaler = StandardScaler
-# dataset[numerical_columns] = scaler.fit_transform(numerical_columns)
+scaler = StandardScaler()
+dataset[numerical_columns] = scaler.fit_transform(numerical_columns)
 
 dataset['year_month'] = dataset['date'].dt.to_period('M')  
 
